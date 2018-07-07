cd4118fdddc3c49f524ad828d7006e921a5e02be
Junki Yamada <geek.junkey@gmail.com>
2018-07-07 13:52:17 +0900

    Gorpの型に直接依存できる状態だったのでインターフェイス化

--- crud/externals/gorp/tables/users.go
+++ crud/externals/gorp/tables/users.go
@@ -17,13 +17,40 @@ type Users struct {
 }
 
 // UsersDao usersテーブルのDAO
-type UsersDao struct {
+type UsersDao interface {
+	// SelectAll 全件取得
+	SelectAll() []Users
+
+	// SelectByID IDでSELECTする
+	// データが取得できた場合はUsers型のデータが設定され、bool型はtrueで返る
+	// データが取得できなかった場合はUsers型は初期値で、bool型はfalseで返る
+	SelectByID(id uint64) (Users, bool)
+
+	// Insert レコードの登録を行う
+	// 登録に成功した場合はtrue、登録に失敗した場合はfalseを返す
+	Insert(user Users) bool
+
+	// Update レコードの更新を行う
+	// 更新に成功した場合はtrue、更新に失敗した場合はfalseを返す
+	Update(user Users) bool
+
+	// DeleteByID IDをキーにレコードの削除を行う
+	// 削除に成功した場合はtrue、削除に失敗した場合はfalseを返す
+	DeleteByID(id uint64) bool
+
+	// SelectNextID 次のIDを発行する
+	// 発行に成功した場合はtrue、発行に失敗した場合はfalseを返す
+	SelectNextID() (uint64, bool)
+}
+
+// UsersDaoGorpImpl usersテーブルのDAO
+type UsersDaoGorpImpl struct {
 	// DBMap DbMap
 	DBMap *gorp.DbMap
 }
 
-// SelectAll 全件取得
-func (dao UsersDao) SelectAll() []Users {
+// SelectAll UsersDaoの実装
+func (dao UsersDaoGorpImpl) SelectAll() []Users {
 
 	var result []Users
 	dao.DBMap.Select(&result, "SELECT * FROM users")
@@ -31,10 +58,8 @@ func (dao UsersDao) SelectAll() []Users {
 	return result
 }
 
-// SelectByID IDでSELECTする
-// データが取得できた場合はUsers型のデータが設定され、bool型はtrueで返る
-// データが取得できなかった場合はUsers型は初期値で、bool型はfalseで返る
-func (dao UsersDao) SelectByID(id uint64) (Users, bool) {
+// SelectByID UsersDaoの実装
+func (dao UsersDaoGorpImpl) SelectByID(id uint64) (Users, bool) {
 
 	r, err := dao.DBMap.Get(Users{}, id)
 
@@ -46,27 +71,24 @@ func (dao UsersDao) SelectByID(id uint64) (Users, bool) {
 	return Users{}, true
 }
 
-// Insert レコードの登録を行う
-// 登録に成功した場合はtrue、登録に失敗した場合はfalseを返す
-func (dao UsersDao) Insert(user Users) bool {
+// Insert UsersDaoの実装
+func (dao UsersDaoGorpImpl) Insert(user Users) bool {
 
 	err := dao.DBMap.Insert(user)
 
 	return err == nil
 }
 
-// Update レコードの更新を行う
-// 更新に成功した場合はtrue、更新に失敗した場合はfalseを返す
-func (dao UsersDao) Update(user Users) bool {
+// Update UsersDaoの実装
+func (dao UsersDaoGorpImpl) Update(user Users) bool {
 
 	_, err := dao.DBMap.Update(user)
 
 	return err == nil
 }
 
-// DeleteByID IDをキーにレコードの削除を行う
-// 削除に成功した場合はtrue、削除に失敗した場合はfalseを返す
-func (dao UsersDao) DeleteByID(id uint64) bool {
+// DeleteByID UsersDaoの実装
+func (dao UsersDaoGorpImpl) DeleteByID(id uint64) bool {
 
 	_, err := dao.DBMap.Exec("DELETE FROM users WHERE id = ?", id)
 
@@ -74,7 +96,7 @@ func (dao UsersDao) DeleteByID(id uint64) bool {
 }
 
 // SelectNextID 次のIDを発行する
-func (dao UsersDao) SelectNextID() (uint64, bool) {
+func (dao UsersDaoGorpImpl) SelectNextID() (uint64, bool) {
 
 	i, error := dao.DBMap.SelectInt("SELECT NEXTVAL('user_id_seq')")
 
