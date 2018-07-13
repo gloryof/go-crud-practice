d40173dee27b07fa3cbd8c0a4ba8cba946cd0aad
Junki Yamada <geek.junkey@gmail.com>
2018-07-13 12:49:44 +0900

    indexページをつか

--- crud/config/staticfile.go
+++ crud/config/staticfile.go
@@ -4,7 +4,9 @@ import (
 	"github.com/labstack/echo"
 )
 
+// SetUpStaticFile 静的ファイルの設定を行う
 func SetUpStaticFile(e *echo.Echo) {
 
 	e.Static("/lib", "./static/lib")
+	e.Static("/index", "./static/index.html")
 }
