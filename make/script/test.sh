91aa8ae4b62b49591c033d26be6ac823d9638b2c
Junki Yamada <geek.junkey@gmail.com>
2018-08-25 10:22:45 +0900

    テスト実行以外もカバレッジを取得するように変更

--- make/script/test.sh
+++ make/script/test.sh
@@ -6,9 +6,10 @@
 
 GOTEST=$1
 BINARY_DIR=$2
+TARGET_DIR=./crud/...
 
 if [ ! -e ${BINARY_DIR} ]; then
     mkdir ${BINARY_DIR}
 fi
 
-${GOTEST} -coverprofile=${BINARY_DIR}/cover.out ./crud/...
+${GOTEST} -coverprofile=${BINARY_DIR}/cover.out -coverpkg=${TARGET_DIR} ${TARGET_DIR}
